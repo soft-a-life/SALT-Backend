0ec5db860529ca369f49083e6f6a22fc8c0979de
harandal24601 <devchaehokim@gmail.com>
2023-06-26 07:35:56 +0900

    feat : Initial setup completed for backend development.

---
+++ src/test/java/SAL/SALT/SaltApplicationTests.java
@@ -0,0 +1,13 @@
+package SAL.SALT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
