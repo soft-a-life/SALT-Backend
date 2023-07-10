83b2390ed1dcc8c2191d549b499910c45b8957d7
harandal24601 <devchaehokim@gmail.com>
2023-07-10 14:41:52 +0900

    style : change annotation style

--- src/main/java/SAL/SALT/repository/UserRepository.java
+++ src/main/java/SAL/SALT/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package SAL.SALT.repository;
 
-import SAL.SALT.entity.User;
+import SAL.SALT.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUserId(Long userId);
 }
