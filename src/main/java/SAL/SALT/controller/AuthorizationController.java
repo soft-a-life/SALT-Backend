0e3aa6273482d9820157fd9238530aac5d8b63fc
harandal24601 <devchaehokim@gmail.com>
2023-07-10 15:15:42 +0900

    refactor :  change mapping & move method
    method called signup are moved to AuthenticationController. And mappings are changed to /api/auth to /api/authorization.

--- src/main/java/SAL/SALT/controller/AuthorizationController.java
+++ src/main/java/SAL/SALT/controller/AuthorizationController.java
@@ -2,16 +2,16 @@ package SAL.SALT.controller;
 
 import SAL.SALT.Entity.User;
 import SAL.SALT.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 /**
  * LoginController
  *
  */
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("api/authorization")
 @CrossOrigin
 public class AuthorizationController {
     private final UserRepository userRepository;
@@ -30,17 +30,6 @@ public class AuthorizationController {
             return ResponseEntity.ok("Login succeed");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User info is incorrect");
-        }
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody User user) {
-        try {
-            // Save data received from signup page to DB
-            userRepository.save(user);
-            return ResponseEntity.ok("Signup succeed");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Signup failed.");
         }
     }
 }
