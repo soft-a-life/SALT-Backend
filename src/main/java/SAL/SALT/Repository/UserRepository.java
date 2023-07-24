ef7f6b73fe0ba746dcdab2aec1e1682de7a1ceb7
harandal24601 <devchaehokim@gmail.com>
2023-07-24 22:01:35 +0900

    chore : delete public

--- src/main/java/SAL/SALT/Repository/UserRepository.java
+++ src/main/java/SAL/SALT/Repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    public Optional<User> findByUserId(String userId);
+    Optional<User> findByUserId(String userId);
 
-    public List<User> findAll();
+    List<User> findAll();
 }
