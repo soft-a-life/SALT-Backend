6a1b79a5b1e969da215a7fc2266a0942e952899c
harandal24601 <devchaehokim@gmail.com>
2023-07-24 13:53:51 +0900

    chore : create User Repo prototype

---
+++ src/main/java/SAL/SALT/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package SAL.SALT.Repository;
+
+import SAL.SALT.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserId(String userId);
+
+    List<User> findAll();
+}
