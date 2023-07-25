3935948ef6c30eb3f62f202e1aefd91f5eaa8cdb
harandal24601 <devchaehokim@gmail.com>
2023-07-25 21:22:27 +0900

    chore : change Member to MemberData
    This domain used to send JSON format to front server

---
+++ src/main/java/SAL/SALT/domain/MemberData.java
@@ -0,0 +1,12 @@
+package SAL.SALT.domain;
+
+public class MemberData {
+    private final String userId;
+    private final String userNickname;
+
+    public MemberData(String userId, String userNickname) {
+        this.userId = userId;
+        this.userNickname = userNickname;
+    }
+
+}
