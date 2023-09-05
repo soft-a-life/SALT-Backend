a6c862fc47575ddd8f942406962a2e1fa08a8aee
harandal24601 <devchaehokim@gmail.com>
2023-09-05 16:59:00 +0900

    feat : add CORS setting
    CORS settings are added and done.

--- src/main/java/SAL/SALT/Configuration/SecurityConfiguration.java
+++ src/main/java/SAL/SALT/Configuration/SecurityConfiguration.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfiguration {
@@ -12,12 +17,27 @@ public class SecurityConfiguration {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors().disable()        // CORS 방지
+                .cors().configurationSource(corsConfigurationSource())       // CORS 방지
+                .and()
                 .csrf().disable()        // CSRF 방지
                 .formLogin().disable()    //기본 로그인 페이지 없애기
                 .headers().frameOptions().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3030"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
     @Bean
